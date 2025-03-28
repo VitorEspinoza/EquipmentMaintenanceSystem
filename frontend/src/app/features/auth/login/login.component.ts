a10d74cad6392987835aaa270ae1d0910335d880
Uriel Duarte de Oliveira <uriel.duarte.oliveira@gmail.com>
2025-03-27 22:59:01 -0300

    Uriel feat(frontend): Add teamplate of login screen

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { AuthService } from '../services/auth.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const MATERIAL_MODULES = [MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatIconModule];
+const FORM_MODULES = [ReactiveFormsModule, FormsModule];
+const COMMON_MODULES = [NgIf];
+const CORE_MODULES = [RouterModule];
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [...MATERIAL_MODULES, ...FORM_MODULES, ...COMMON_MODULES, ...CORE_MODULES],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -14,16 +26,16 @@ export class LoginComponent implements OnInit {
   private readonly router = inject(Router);
 
   private readonly authService = inject(AuthService);
-  loginForm!: FormGoup;
+  loginForm!: FormGroup;
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      pessword: ['', [Validators.required]],
+      password: ['', [Validators.required]],
     });
   }
 
-  OnSubmit() {
+  onSubmit() {
     this.authService.login(this.loginForm.value).subscribe({
       next: () => console.log('Successful'),
       error: () => console.log('Error'),
