5560d04f4bca328a380f02579d7bd9c79cd37bfb
Rafael <rafaelleonardodasilva.dv@gmail.com>
2025-04-02 12:22:56 -0300

    RAFAEL fix(frontent): change import path for intercerptor on app.config.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,14 +2,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
-import { authInterceptor } from './core/services/interceptors/auth.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),  
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideEnvironmentNgxMask(),
   ],
 };
