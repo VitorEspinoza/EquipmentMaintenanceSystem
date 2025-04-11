5ebb73d2dcd56e7d3abd38a2d5f86e915c9f5f14
ianBAlmeida <bailoneian@gmail.com>
2025-04-10 22:10:27 -0300

    IAN feat(feature/frontend-employee-budget): Create bugdet page components

---
+++ frontend/src/app/features/employee/employee-budget/employee-budget.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-budget',
+  imports: [],
+  templateUrl: './employee-budget.component.html',
+  styleUrl: './employee-budget.component.css'
+})
+export class EmployeeBudgetComponent {
+
+}
--- frontend/src/app/features/employee/employee.routes.ts
+++ frontend/src/app/features/employee/employee.routes.ts
@@ -4,6 +4,6 @@ export const employeeRoutes: Routes = [
   {
     path: 'employee-home',
     loadComponent: () =>
-      import('./employee-home/employee-home/employee-home.component').then(m => m.EmployeeHomeComponent),
+      import('./employee-home/employee-home.component').then(m => m.EmployeeHomeComponent),
   },
 ];
