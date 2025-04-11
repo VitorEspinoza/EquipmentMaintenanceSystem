7097c079d4f9b571a26b3f67b1bcc4760e66e53a
Vitor <vitor.espinozadg@gmail.com>
2025-04-11 14:18:10 -0300

    fix(backend):fixes private constructor error

--- backend/src/main/java/br/com/backend/backend/Entities/EquipmentCategory.java
+++ backend/src/main/java/br/com/backend/backend/Entities/EquipmentCategory.java
@@ -1,18 +1,27 @@
 package br.com.backend.backend.Entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "equipment_category")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 
 @Getter
 public class EquipmentCategory {
+
+    private EquipmentCategory(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.active = true;
+        this.equipments = new HashSet<>();
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,7 +41,7 @@ public class EquipmentCategory {
     private Set<Equipment> equipments;
 
     public static EquipmentCategory create(String name, String description) {
-        return new EquipmentCategory(null, name, description, true, new HashSet<>());
+        return new EquipmentCategory(name, description);
     }
 
     public void update(String name, String description) {
