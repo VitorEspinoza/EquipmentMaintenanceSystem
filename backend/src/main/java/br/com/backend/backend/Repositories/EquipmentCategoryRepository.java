7c28d1dc0992aabb3adbd8d7579abaa4961ceca9
Vitor <vitor.espinozadg@gmail.com>
2025-04-11 16:21:10 -0300

    feat(backend): add repository methods

--- backend/src/main/java/br/com/backend/backend/Repositories/EquipmentCategoryRepository.java
+++ backend/src/main/java/br/com/backend/backend/Repositories/EquipmentCategoryRepository.java
@@ -1,8 +1,21 @@
 package br.com.backend.backend.Repositories;
-
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
 import br.com.backend.backend.Entities.EquipmentCategory;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EquipmentCategoryRepository extends JpaRepository<EquipmentCategory, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface EquipmentCategoryRepository extends JpaRepository<EquipmentCategory, Integer> {
+    Optional<EquipmentCategory> findByNameIgnoreCase(String name);
+
+    @NonNull
+    @EntityGraph(attributePaths = "equipments")
+    Optional<EquipmentCategory> findById(Integer id);
+
+    @NonNull
+    @EntityGraph(attributePaths = "equipments")
+    List<EquipmentCategory> findAll();
 }
