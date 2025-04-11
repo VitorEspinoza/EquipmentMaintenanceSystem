689b2819c111fbd0501138a387b0cd8570af906d
Vitor <vitor.espinozadg@gmail.com>
2025-04-11 14:15:50 -0300

    feat(backend): creates category repository

---
+++ backend/src/main/java/br/com/backend/backend/Repositories/EquipmentCategoryRepository.java
@@ -0,0 +1,8 @@
+package br.com.backend.backend.Repositories;
+
+import br.com.backend.backend.Entities.EquipmentCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipmentCategoryRepository extends JpaRepository<EquipmentCategory, Integer> {
+
+}
