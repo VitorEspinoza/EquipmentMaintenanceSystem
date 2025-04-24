1e61cd4c21eab0620d2847f7ff78461ff59b9128
Vitor <vitor.espinozadg@gmail.com>
2025-04-24 10:56:21 -0300

    feat(frontend): Creates layout with sidenav component and behavior

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
+import { ContentComponent } from './features/layout/main/content.component';
+import { SidenavComponent } from './features/layout/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgxMaskDirective, NgxMaskPipe],
+  imports: [RouterOutlet, NgxMaskDirective, NgxMaskPipe, SidenavComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EquipmentMaintenanceSystem';
+  isSidebarCollapsed = signal<boolean>(false);
+  screenWidth = signal<number>(window.innerWidth);
+
+  @HostListener('window:resize')
+  onResize() {
+    this.screenWidth.set(window.innerWidth);
+    if (this.screenWidth() < 768) {
+      this.isSidebarCollapsed.set(true);
+    }
+  }
+
+  ngOnInit(): void {
+    this.isSidebarCollapsed.set(this.screenWidth() < 768);
+  }
+
+  changeIsSidebarCollapsed(isSidebarCollapsed: boolean): void {
+    this.isSidebarCollapsed.set(isSidebarCollapsed);
+  }
 }
