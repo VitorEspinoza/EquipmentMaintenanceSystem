a2255f851049f7f4f7c83423d2eab862ddc5d6a9
Vitor <vitor.espinozadg@gmail.com>
2025-05-02 15:35:04 -0300

    feat(backend): add result pattern

--- backend/src/main/java/br/com/backend/backend/Services/EquipmentCategoryService.java
+++ backend/src/main/java/br/com/backend/backend/Services/EquipmentCategoryService.java
@@ -2,9 +2,11 @@ package br.com.backend.backend.Services;
 
 import br.com.backend.backend.DTOs.EquipmentCategoryInputDTO;
 import br.com.backend.backend.DTOs.EquipmentCategoryResponseDTO;
+import br.com.backend.backend.DTOs.ResultViewModel;
 import br.com.backend.backend.Entities.EquipmentCategory;
 import br.com.backend.backend.Exceptions.Custom.CategoryAlreadyExists;
 import br.com.backend.backend.Exceptions.Custom.EquipmentCategoryNotFoundException;
+import br.com.backend.backend.Mappers.EquipmentCategoryMapper;
 import br.com.backend.backend.Repositories.EquipmentCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,42 +19,50 @@ import java.util.List;
 public class EquipmentCategoryService {
 
     private final EquipmentCategoryRepository repository;
+    private final EquipmentCategoryMapper mapper;
 
-    public EquipmentCategoryService(EquipmentCategoryRepository repository) {
+    public EquipmentCategoryService(EquipmentCategoryRepository repository, EquipmentCategoryMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
 
     @Transactional(readOnly = true)
-    public List<EquipmentCategoryResponseDTO> getAllCategories() {
+    public ResultViewModel<List<EquipmentCategoryResponseDTO>> getAllCategories() {
         var categories = repository.findAll();
-        return categories.stream()
-                .map(EquipmentCategoryResponseDTO::fromEntity)
-                .toList();
+
+        return ResultViewModel.success(mapper.toDtoList(categories));
     }
+
     @Transactional(readOnly = true)
-    public EquipmentCategoryResponseDTO getCategoryById(Integer id) {
+    public ResultViewModel<EquipmentCategoryResponseDTO> getCategoryById(Integer id) {
         var category = repository.findById(id).orElseThrow(() -> new EquipmentCategoryNotFoundException(id));
-        return EquipmentCategoryResponseDTO.fromEntity(category);
+
+        return ResultViewModel.success(mapper.toDto(category));
     }
 
     @Transactional
-    public EquipmentCategory createCategory(EquipmentCategoryInputDTO equipmentCategoryInputDTO) {
-        var categoryName = equipmentCategoryInputDTO.name();
+    public ResultViewModel<EquipmentCategoryResponseDTO> createCategory(EquipmentCategoryInputDTO dto) {
+        var categoryName = dto.name();
+
         if (repository.findByNameIgnoreCase(categoryName).isPresent()) {
             throw new CategoryAlreadyExists(categoryName);
         }
-        var category = EquipmentCategory.create(categoryName, equipmentCategoryInputDTO.description());
-        return repository.save(category);
+
+        var category = mapper.toEntity(dto);
+        var savedCategory = repository.save(category);
+
+        return ResultViewModel.success(mapper.toDto(savedCategory));
     }
 
     @Transactional
-    public EquipmentCategory updateCategory(Integer id, EquipmentCategoryInputDTO equipmentCategoryInputDTO) {
+    public ResultViewModel<EquipmentCategoryResponseDTO> updateCategory(Integer id, EquipmentCategoryInputDTO dto) {
         var category = repository.findById(id).orElseThrow(() -> new EquipmentCategoryNotFoundException(id));
 
-        category.update(equipmentCategoryInputDTO.name(), equipmentCategoryInputDTO.description());
+        category.update(dto.name(), dto.description());
+        var updatedCategory = repository.save(category);
 
-        return repository.save(category);
+        return ResultViewModel.success(mapper.toDto(updatedCategory));
     }
 
     @Transactional
