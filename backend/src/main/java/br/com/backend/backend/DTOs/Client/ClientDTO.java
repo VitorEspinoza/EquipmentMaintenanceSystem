88f7a1caa1c7dfcd774bd4123164ea84b092493e
Vitor <vitor.espinozadg@gmail.com>
2025-05-26 17:04:46 -0300

    feat(backend): add mapping for client dto

--- backend/src/main/java/br/com/backend/backend/DTOs/Client/ClientDTO.java
+++ backend/src/main/java/br/com/backend/backend/DTOs/Client/ClientDTO.java
@@ -1,13 +1,12 @@
 package br.com.backend.backend.DTOs.Client;
 
 import br.com.backend.backend.DTOs.Address.AddressDTO;
+import br.com.backend.backend.Entities.Client;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 public class ClientDTO {
@@ -19,4 +18,16 @@ public class ClientDTO {
     private String role;
     private String phone;
     private AddressDTO address;
+    
+    public static ClientDTO fromEntity(Client client) {
+        return new ClientDTO(
+                client.getId(),
+                client.getAccount().getId(),
+                client.getName(),
+                client.getAccount().getEmail(),
+                client.getAccount().getRole(),
+                client.getPhone(),
+                AddressDTO.fromEntity(client.getAddress())
+        );
+    }
 }
