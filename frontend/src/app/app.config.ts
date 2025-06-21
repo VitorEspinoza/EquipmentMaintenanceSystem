e18ba3e0d6522dc0da6675bdb51e5aa130c1912b
Vitor <vitor.espinozadg@gmail.com>
2025-06-21 18:44:32 -0300

    feat: adds pagination option in dynamic table

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { ApplicationConfig, inject, LOCALE_ID, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { MAT_DATE_FORMATS, provideNativeDateAdapter } from '@angular/material/core';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
 import { catchError, EMPTY } from 'rxjs';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { AuthService } from './features/auth/services/auth.service';
+import { MatPaginatorIntlPtBr } from './shared/services/mat-paginator-intl-pt-br.service';
 
 export const BR_DATE_FORMATS = {
   parse: {
@@ -29,8 +31,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideEnvironmentNgxMask(),
-    { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideNativeDateAdapter(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: MatPaginatorIntl, useClass: MatPaginatorIntlPtBr },
     { provide: MAT_DATE_FORMATS, useValue: BR_DATE_FORMATS },
     provideAppInitializer(() =>
       inject(AuthService)
--- frontend/src/app/shared/components/dynamic-table/dynamic-table.component.ts
+++ frontend/src/app/shared/components/dynamic-table/dynamic-table.component.ts
@@ -1,29 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, input, output } from '@angular/core';
+import { AfterViewInit, Component, computed, effect, input, output, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { TableAction, TableColumn } from '../../models/TableColumn';
-
 @Component({
   selector: 'app-dynamic-table',
-  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule, MatTooltipModule, RouterModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    RouterModule,
+    MatPaginatorModule,
+  ],
   templateUrl: './dynamic-table.component.html',
   styleUrl: './dynamic-table.component.css',
 })
-export class DynamicTableComponent<TAction = any, TData = any> {
+export class DynamicTableComponent<TAction = any, TData = any> implements AfterViewInit {
   dataSource = input.required<TData[]>();
   columns = input.required<TableColumn[]>();
   tableClass = input('mat-elevation-z8 w-full');
   rowClass = input('hover:bg-gray-50 transition-colors');
   emptyMessage = input('Nenhum registro encontrado');
+  showPaginator = input(false);
 
   badgeClassResolver = input<(element: TData, columnKey: string) => string>();
   actionsResolver = input<(element: TData) => TableAction<TAction>[]>();
 
   actionClicked = output<{ tableAction: TableAction<TAction>; element: TData }>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  internalDataSource = new MatTableDataSource<any>();
 
   displayedColumns = computed(() => this.columns().map(col => col.key as string));
 
@@ -41,6 +54,16 @@ export class DynamicTableComponent<TAction = any, TData = any> {
       };
     });
   });
+
+  constructor() {
+    effect(() => {
+      this.internalDataSource.data = this.processedData();
+    });
+  }
+
+  ngAfterViewInit() {
+    this.internalDataSource.paginator = this.paginator;
+  }
 
   private processRowData(item: TData, columns: TableColumn[]): Record<string, any> {
     const processed: Record<string, any> = {};
